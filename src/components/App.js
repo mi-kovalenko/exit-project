0e41c1f405841c3c332fa01bea792b996bc726d3
Maria <mi.kovalenko@mail.ru>
2023-08-25 21:54:34 +0300

    .

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import '../scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <code>технические работы</code>
+      </header>
+    </div>
+  );
+}
+
+export default App;
