c546f648626f177375e546d0469f2ee874aa2868
Maria <mi.kovalenko@mail.ru>
2023-08-25 21:59:26 +0300

    .

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <code>технические работы</code>
+        <code>звучит грустновато</code>
       </header>
     </div>
   );
